2399e61a930b5a1602d4ff4b203d3557526aa4e3
rgarro <rgarro@gmail.com>
2023-04-04 14:14:36 -0500

    project uptdate ..

---
+++ Assets/Scripts/BatComputer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+*      _..--""````""--.._
+*    .'       |\/|       '.
+*   /    /`._ |  | _.'\    \
+*  ;    /              \    |
+*  |   /                \   |
+*  ;  / .-.          .-. \  ;
+*   \ |/   \.-.  .-./   \| /
+*    '._       \/       _.'
+*       ''--..____..--''
+* 
+*
+*@author Rolando<rolando@emptyart.xyz>
+*/
+public class BatComputer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
