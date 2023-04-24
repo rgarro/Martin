351937f06224e2a042eeec517843592f6dbd9ab3
rgarro <rgarro@gmail.com>
2023-04-24 04:36:01 -0500

    out kitesurfing VIII..

--- Assets/Scripts/BatComputer.cs
+++ Assets/Scripts/BatComputer.cs
@@ -11,7 +11,15 @@ using UnityEngine;
 *   \ |/   \.-.  .-./   \| /
 *    '._       \/       _.'
 *       ''--..____..--''
-* 
+*  Mataron a Cachudo el de la Leon IIIX
+* Papelote el de Tibas existio, Mamey era otro el , planetario vende mota en el pollo
+* Mamerto cree que es jacson, ya murio varo el del bar mexico, chisco hecho a charly del parqueo de pali
+* Tego Calde resiste fumar piedra 14 meses mas, Nazis del Ebais inoculan diabetes en 15 ciudadanos de colima
+* La Galeta esta por el puente de abajo del virilla, ahi deje una onza de mota enterrada ...
+* Jhonny Araya prendio la batisenal y los dominicanos mataron un piedrero
+* Pilo sera el alcalde de tibas , mataron a vladi afuera de un putero en chepe
+*
+*
 *
 *@author Rolando<rolando@emptyart.xyz>
 */
