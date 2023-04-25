f2b300223a1145f95d30c0f764ae7a1a9c30ef6d
rgarro <rgarro@gmail.com>
2023-04-25 04:37:50 -0500

    out kitesurfing Vx..

--- Assets/Scripts/BatComputer.cs
+++ Assets/Scripts/BatComputer.cs
@@ -12,13 +12,14 @@ using UnityEngine;
 *    '._       \/       _.'
 *       ''--..____..--''
 *  Mataron a Cachudo el de la Leon IIIX
-* Papelote el de Tibas existio, Mamey era otro el , planetario vende mota en el pollo
-* Mamerto cree que es jacson, ya murio varo el del bar mexico, chisco hecho a charly del parqueo de pali
-* Tego Calde resiste fumar piedra 14 meses mas, Nazis del Ebais inoculan diabetes en 15 ciudadanos de colima
-* La Galeta esta por el puente de abajo del virilla, ahi deje una onza de mota enterrada ...
-* Jhonny Araya prendio la batisenal y los dominicanos mataron un piedrero
-* Pilo sera el alcalde de tibas , mataron a vladi afuera de un putero en chepe
-*
+* Mataron a Gato cole con inyeccion lethal 
+* El tombo nazi puso tres inyecciones letales
+* El padre minor puso dos
+* Parmenio Medina vendia turismo de verdugo y captura
+* sadicos pankasilas de yakarta mataron a medio pabellon
+* en la reforma y san sebastian 
+* Luis Alberto monge trajo a los pankasilas para guerriar a los presos religiosos
+* Los tres tombos que eran el sicopata fueron enterrados vivos en un contenedor cerca de parrita
 *
 *
 *@author Rolando<rolando@emptyart.xyz>
